9ede984ec3168e5a0b22843d1f86c25a4762e79f
rlouiseeee <161690210+rlouiseeee@users.noreply.github.com>
2024-03-01 17:58:56 +0800

    edit

--- resources/js/Pages/Project/Index.vue
+++ resources/js/Pages/Project/Index.vue
@@ -7,6 +7,7 @@ import ConfirmationModal from '@/Components/ConfirmationModal.vue';
 import DangerButton from '@/Components/DangerButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 
+const isVisible = ref(false);
 
 const sampleData = ref([
     {
@@ -73,10 +74,11 @@ const openDeleteModal = (name, date) => {
 }
 
 
+
 const openAddModal = (name, date) => {
     project_Name.value.name = name
     project_Name.value.date = date
-    deleteModal.value = true
+    isVisible.value = true
     const now = new Date();
     const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
     const hours = now.getHours() % 12 || 12;
@@ -85,7 +87,6 @@ const openAddModal = (name, date) => {
 }
 
 
-    
 
 
 
@@ -102,7 +103,7 @@ const openAddModal = (name, date) => {
             </div>
             <div class="absolute fixed top-[9vmin] right-14">
                 <button type="button "
-                    class=" flex flex-wrap text-white bg-[#f39202] hover:bg-gray-950  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
+                    class=" flex flex-wrap text-white bg-[#f39202]  hover:bg-gray-950  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
                     Create New Project
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 14 10">
@@ -128,12 +129,93 @@ const openAddModal = (name, date) => {
                             class="text-white text-md   transition ease-in-out delay-150 border-4 border-gray-600 hover:border-gray-300 ease-in-out delay-150 hover:scale-[100%] duration-200 o">
                             WAPS</p>
                         <div class="absolute bottom-4 left-[50%] translate-x-[-50%] rounded-lg ">
-                            <button type="button" 
-                                class=" flex flex-wrap w-[max-content] text-white bg-[#3ca9e0] hover:bg-[#1e71b8] font-semibold text-sm px-5 py-2.5 text-center">
-                                Add Project 
+
+
+                            <!-- Modal toggle -->
+                            <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
+                                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                type="button" @click="openAddModal">
+                                Add Project
                             </button>
 
-                           
+                            <!-- Main modal -->
+                            <div id="crud-modal" v-show="isVisible" tabindex="-1" aria-hidden="true"
+                                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                <div class="relative p-4 w-full max-w-md max-h-full">
+                                    <!-- Modal content -->
+                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <!-- Modal header -->
+                                        <div
+                                            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                                Create Sub Project
+                                            </h3>
+                                            <button type="button"
+                                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                data-modal-toggle="crud-modal">
+                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none" viewBox="0 0 14 14">
+                                                    <path stroke="currentColor" stroke-linecap="round"
+                                                        stroke-linejoin="round" stroke-width="2"
+                                                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                </svg>
+                                                <span class="sr-only">Close</span>
+                                            </button>
+                                        </div>
+                                        <!-- Modal body -->
+                                        <form class="p-4 md:p-5">
+                                            <div class="grid gap-4 mb-4 grid-cols-2">
+                                                <div class="col-span-2">
+                                                    <label for="name"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                                    <input type="text" name="name" id="name"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                        placeholder="Type product name" required="">
+                                                </div>
+                                                <div class="col-span-2 sm:col-span-1">
+                                                    <label for="price"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                                    <input type="number" name="price" id="price"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                        placeholder="$2999" required="">
+                                                </div>
+                                                <div class="col-span-2 sm:col-span-1">
+                                                    <label for="category"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sub Project</label>
+                                                    <select id="category"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                        <option selected="">Select category</option>
+                                                        <option value="TV">TV/Monitors</option>
+                                                        <option value="PC">PC</option>
+                                                        <option value="GA">Gaming/Console</option>
+                                                        <option value="PH">Phones</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-span-2">
+                                                    <label for="description"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
+                                                        Description</label>
+                                                    <textarea id="description" rows="4"
+                                                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                        placeholder="Write product description here"></textarea>
+                                                </div>
+                                            </div>
+                                            <button type="submit"
+                                                class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd"
+                                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                        clip-rule="evenodd"></path>
+                                                </svg>
+                                                Add new product
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+
 
                             <div class="flex flex-wrap justify-center gap-2 mt-4 text-[#ffffff]">
                                 <button aria-label="Share this post" type="button"
@@ -175,29 +257,28 @@ const openAddModal = (name, date) => {
                     <div
                         class="mr-1 mb-1 px-2 py-1 text-md bg-white border rounded text-gray-400 ml-auto hover:bg-blue-900 hover:text-white consor-pointer">
                         <span>Next »</span>
-                    </div>
                 </div>
             </div>
-            <ConfirmationModal :show="deleteModal" @close="!deleteModal">
-                <template #title>
-                    Delete API Token
-                </template>
-                <template #content>
-                    Are you sure you would like to delete {{ project_Name.name }}? <br>
-                    <small>{{ project_Name.date }}</small>
-                </template>
-
-                <template #footer>
-                    <SecondaryButton @click="deleteModal = false">
-                        Cancel
-                    </SecondaryButton>
-                    <DangerButton class="ms-3" @click="_deleteProject('John Doe')">
-                        Delete
-                    </DangerButton>
-                </template>
-            </ConfirmationModal>
-
-
         </div>
+        <ConfirmationModal :show="deleteModal" @close="!deleteModal">
+            <template #title>
+                Delete API Token
+            </template>
+            <template #content>
+                Are you sure you would like to delete {{ project_Name.name }}? <br>
+                <small>{{ project_Name.date }}</small>
+            </template>
+
+            <template #footer>
+                <SecondaryButton @click="deleteModal = false">
+                    Cancel
+                </SecondaryButton>
+                <DangerButton class="ms-3" @click="_deleteProject('John Doe')">
+                    Delete
+                </DangerButton>
+            </template>
+        </ConfirmationModal>
+
+    </div>
 </AppLayout></template>
 
